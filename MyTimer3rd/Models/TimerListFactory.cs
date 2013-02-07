1887245bd8d33aae4b4e0a3271ffb50d2ae97e75
ishikura <ishikura@yk.rim.or.jp>
2013-02-07 16:52:37 +0900

    Factory class(Timer counter list) add.

---
+++ MyTimer3rd/Models/TimerListFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyTimer3rd.Models
+{
+    public static class TimerListFactory
+    {
+        public static TimerValueListModel Create()
+        {
+            return TimerValueListModel.Instance();
+        }
+    }
+}
